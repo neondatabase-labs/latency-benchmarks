41a50a0de119529fcfdc5dcc5ce85d3f8a59d4b0
andrelandgraf <andre.timo.landgraf@gmail.com>
2025-04-30 12:25:12 -0700

    fix DB_URL not set during build

--- cron-vercel/lib/meta-db.ts
+++ cron-vercel/lib/meta-db.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import { neon } from '@neondatabase/serverless';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL!);
 
 interface Database {
   id: number;
