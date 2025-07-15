ba07c3b49e3e43e4886b7b854a31e184141daa2a
Andre Landgraf <andre.timo.landgraf@gmail.com>
2025-07-14 17:05:26 -0700

    support rewrite from demos/regional-latency to link from neon.com/demos/regional-latency

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/demos/regional-latency",
+        destination: "/",
+      },
+      {
+        source: "/demos/regional-latency/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
